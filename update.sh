67689a8c8c28a3898ccc161111d0900db120f44e
stakor <chris.stakor@gmail.com>
2018-07-01 12:34:34 -0400

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update all the things...
+# Description:
+# Update all packages on Ubuntu system as much as possible, automagically.
+#
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
