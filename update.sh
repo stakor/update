cecbe91c32cb1a9b2b6d35025abff6dfc795b153
Stakor <chris.stakor@gmail.com>
2021-04-13 09:51:47 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,4 @@
 # Update all packages on Ubuntu system as much as possible, automagically.
 #
 
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get autoremove -y
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y
